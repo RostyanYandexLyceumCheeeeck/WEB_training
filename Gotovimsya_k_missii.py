58b3956e3691b0ae2128dff74793c662f127cbe5
Rostislav <zagigtovr97@gmail.com>
2023-02-24 11:53:13 +0500

    Задание 2. Тренировки в полёте !

--- Gotovimsya_k_missii.py
+++ Gotovimsya_k_missii.py
@@ -1,6 +1,19 @@
 from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
+profession = {
+    'врач': ['Научные симуляторы', 'img/doctor.jpg'],
+    'повар': ['Научные симуляторы', 'img/chef.jpg'],
+    'военный': ['Научные симуляторы', 'img/soldier.jpg'],
+    'охранник': ['Инженерные тренажеры', 'img/security.jpg'],
+    'физик-ядерщик': ['Научные симуляторы', 'img/nuclear physicist.jpg'],
+    'электрик': ['Инженерные тренажеры', 'img/electrician.jpg'],
+    'грузчик': ['Инженерные тренажеры', 'img/loader.jpg'],
+    'оператор': ['Инженерные тренажеры', 'img/operator.jpg'],
+    'слесарь': ['Инженерные тренажеры', 'img/plumber.jpg'],
+    'программист': ['Инженерные тренажеры', 'img/programmer.jpg'],
+    'растение': ['Научные симуляторы', 'img/plant.jpg'],
+}
 
 
 @app.route('//<title>')
@@ -9,5 +22,12 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def profil(prof):
+    who, name = profession[prof]
+    return render_template('prof_scheme.html', prof=who, scheme=url_for('static', filename=name))
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
+'127.0.0.1:8080/training/врач'
